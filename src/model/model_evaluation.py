56c3e055904318947d1e35c46b4cea475dae8e9d
ShashankraiOO7 <shashankrai020700@gmail.com>
2025-03-28 01:24:09 +0530

    Mlflowserver Setup with S3

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -7,6 +7,11 @@ import logging
 from sklearn.metrics import classification_report
 from sklearn.feature_extraction.text import TfidfVectorizer
 import os
+import mlflow
+import mlflow.sklearn
+import yaml
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # logging configuration
 logger = logging.getLogger('model_evaluation')
@@ -89,35 +94,94 @@ def get_root_directory() -> str:
     return os.path.abspath(os.path.join(current_dir, '../../'))
 
 
-def main():
+def log_confusion_matrix(cm, dataset_name):
+    """Log confusion matrix as an artifact."""
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    plt.title(f'Confusion Matrix for {dataset_name}')
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+
+    # Save confusion matrix plot as a file and log it to MLflow
+    cm_file_path = f'confusion_matrix_{dataset_name}.png'
+    plt.savefig(cm_file_path)
+    mlflow.log_artifact(cm_file_path)
+    plt.close()
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
     try:
-        # Get root directory
-        root_dir = get_root_directory()
-
-        # Load the model and vectorizer from the root directory
-        model = load_model(os.path.join(root_dir, 'lgbm_model.pkl'))
-        vectorizer = load_vectorizer(os.path.join(root_dir, 'tfidf_vectorizer.pkl'))
-
-        # Load the training data and test data from the interim directory
-        train_data = load_data(os.path.join(root_dir, 'data/interim/train_processed.csv'))
-        test_data = load_data(os.path.join(root_dir, 'data/interim/test_processed.csv'))
-
-        # Extract features using the loaded TF-IDF vectorizer
-        X_train_tfidf = vectorizer.transform(train_data['clean_comment'].values)
-        y_train = train_data['category'].values
-
-        X_test_tfidf = vectorizer.transform(test_data['clean_comment'].values)
-        y_test = test_data['category'].values
-
-        # Evaluate on training data
-        evaluate_model(model, X_train_tfidf, y_train, "Training Data")
-
-        # Evaluate on test data
-        evaluate_model(model, X_test_tfidf, y_test, "Test Data")
-
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters loaded from %s', params_path)
+        return params
     except Exception as e:
-        logger.error(f"Failed to complete model evaluation: {e}")
-        print(f"Error: {e}")
+        logger.error('Error loading parameters from %s: %s', params_path, e)
+        raise
+def main():
+    mlflow.set_tracking_uri("http://ec2-3-110-174-70.ap-south-1.compute.amazonaws.com:5000/")
+
+    mlflow.set_experiment('dvc-pipeline-runs')
+    
+    with mlflow.start_run():
+        try:
+            # Get root directory
+            root_dir = get_root_directory()
+            params = load_params(os.path.join(root_dir,'params.yaml'))
+            
+            for key, value in params.items():
+                mlflow.log_param(key,value)
+                
+
+            # Load the model and vectorizer from the root directory
+            model = load_model(os.path.join(root_dir, 'lgbm_model.pkl'))
+            vectorizer = load_vectorizer(os.path.join(root_dir, 'tfidf_vectorizer.pkl'))
+            
+            # Log model parameters
+            if hasattr(model, 'get_params'):
+                for param_name, param_value in model.get_params().items():
+                    mlflow.log_param(param_name, param_value)
+            
+            
+             # Log model and vectorizer
+            mlflow.sklearn.log_model(model, "lgbm_model")
+            mlflow.log_artifact(os.path.join(root_dir, 'tfidf_vectorizer.pkl'))
+            
+            
+            # Load the training data and test data from the interim directory
+            train_data = load_data(os.path.join(root_dir, 'data/interim/train_processed.csv'))
+            test_data = load_data(os.path.join(root_dir, 'data/interim/test_processed.csv'))
+
+            # Extract features using the loaded TF-IDF vectorizer
+            X_train_tfidf = vectorizer.transform(train_data['clean_comment'].values)
+            y_train = train_data['category'].values
+
+            X_test_tfidf = vectorizer.transform(test_data['clean_comment'].values)
+            y_test = test_data['category'].values
+
+            # Evaluate on training data
+            report_train,cm_train=evaluate_model(model, X_train_tfidf, y_train, "Training Data")
+
+            # Evaluate on test data
+            report_test,cm_test=evaluate_model(model, X_test_tfidf, y_test, "Test Data")
+            for label, metrics in report_train.items():
+                if isinstance(metrics, dict):
+                    mlflow.log_metrics({
+                        f"train_{label}_precision": metrics['precision'],
+                        f"train_{label}_recall": metrics['recall'],
+                        f"train_{label}_f1-score": metrics['f1-score']
+                    })
+            for label, metrics in report_test.items():
+                if isinstance(metrics, dict):
+                    mlflow.log_metrics({
+                        f"test_{label}_precision": metrics['precision'],
+                        f"test_{label}_recall": metrics['recall'],
+                        f"test_{label}_f1-score": metrics['f1-score']
+                    })
+            log_confusion_matrix(cm_train, "Train Data")
+            log_confusion_matrix(cm_test, "Test Data")
+        except Exception as e:
+            logger.error(f"Failed to complete model evaluation: {e}")
+            print(f"Error: {e}")
 
 
 if __name__ == '__main__':
